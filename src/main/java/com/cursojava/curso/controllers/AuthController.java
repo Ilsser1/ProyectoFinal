53b30e44f7560ed7e717f03c5a10f373161247e2
Ilsser1 <iguatemalam@miumg.edu.gt>
2024-05-31 22:34:58 -0600

    Proyecto final subido
    illsser

---
+++ src/main/java/com/cursojava/curso/controllers/AuthController.java
@@ -0,0 +1,36 @@
+
+// en esta guardamos el area de contraseñas y segurudad para las autitaciones
+package com.cursojava.curso.controllers;
+
+import com.cursojava.curso.dao.UsuarioDao;
+import com.cursojava.curso.models.Usuario;
+import com.cursojava.curso.utils.JWTUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//AQUI PUEDO DARLE ACCESO A JWT A TD EL PROYECT
+
+@RestController
+public class AuthController {
+
+    @Autowired
+    private UsuarioDao usuarioDao;
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @RequestMapping(value = "api/login", method = RequestMethod.POST)
+    public String login(@RequestBody Usuario usuario) {
+
+        Usuario usuarioLogueado = usuarioDao.obtenerUsuarioPorCredenciales(usuario);
+        if (usuarioLogueado != null) {
+            String tokenJwt = jwtUtil.create(String.valueOf(usuarioLogueado.getId()), usuarioLogueado.getEmail());
+            return tokenJwt;
+        }
+        return "FAIL";
+    }
+
+}
